38c7cff3f55c7db904f3a8e04ccf8c07cf45c97e
MustafaSeyrek <mustafa.seyrek@ceng.deu.edu.tr>
2021-02-20 19:11:01 +0300

    code files

---
+++ Assignment1/Assignment1/Form1.Designer.cs
@@ -0,0 +1,121 @@
+﻿namespace Assignment1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtPath = new System.Windows.Forms.TextBox();
+            this.btnBrowse = new System.Windows.Forms.Button();
+            this.lblResult = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnDiscover = new System.Windows.Forms.Button();
+            this.lblResult2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // txtPath
+            // 
+            this.txtPath.Location = new System.Drawing.Point(30, 76);
+            this.txtPath.Name = "txtPath";
+            this.txtPath.Size = new System.Drawing.Size(500, 22);
+            this.txtPath.TabIndex = 1;
+            // 
+            // btnBrowse
+            // 
+            this.btnBrowse.Location = new System.Drawing.Point(553, 70);
+            this.btnBrowse.Name = "btnBrowse";
+            this.btnBrowse.Size = new System.Drawing.Size(144, 34);
+            this.btnBrowse.TabIndex = 2;
+            this.btnBrowse.Text = "BROWSE";
+            this.btnBrowse.UseVisualStyleBackColor = true;
+            this.btnBrowse.Click += new System.EventHandler(this.btnBrowse_Click);
+            // 
+            // lblResult
+            // 
+            this.lblResult.AutoSize = true;
+            this.lblResult.ForeColor = System.Drawing.Color.Red;
+            this.lblResult.Location = new System.Drawing.Point(27, 123);
+            this.lblResult.Name = "lblResult";
+            this.lblResult.Size = new System.Drawing.Size(0, 17);
+            this.lblResult.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
+            this.label1.Location = new System.Drawing.Point(136, 20);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(407, 17);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "2016510060 - Mustafa SEYREK / 2016510052 - Enes KİRİKTİR";
+            // 
+            // btnDiscover
+            // 
+            this.btnDiscover.Location = new System.Drawing.Point(553, 172);
+            this.btnDiscover.Name = "btnDiscover";
+            this.btnDiscover.Size = new System.Drawing.Size(144, 41);
+            this.btnDiscover.TabIndex = 5;
+            this.btnDiscover.Text = "DISCOVER";
+            this.btnDiscover.UseVisualStyleBackColor = true;
+            this.btnDiscover.Click += new System.EventHandler(this.btnDiscover_Click);
+            // 
+            // lblResult2
+            // 
+            this.lblResult2.AutoSize = true;
+            this.lblResult2.ForeColor = System.Drawing.Color.Red;
+            this.lblResult2.Location = new System.Drawing.Point(550, 238);
+            this.lblResult2.Name = "lblResult2";
+            this.lblResult2.Size = new System.Drawing.Size(0, 17);
+            this.lblResult2.TabIndex = 6;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(838, 328);
+            this.Controls.Add(this.lblResult2);
+            this.Controls.Add(this.btnDiscover);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.lblResult);
+            this.Controls.Add(this.btnBrowse);
+            this.Controls.Add(this.txtPath);
+            this.Name = "Form1";
+            this.Text = "DEUCENG - ML Classification Tool";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.TextBox txtPath;
+        private System.Windows.Forms.Button btnBrowse;
+        private System.Windows.Forms.Label lblResult;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnDiscover;
+        private System.Windows.Forms.Label lblResult2;
+    }
+}
+
